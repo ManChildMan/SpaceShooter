70455db14bd4be8625e91983c20c3a4314da4ddd
Nathaniel Fuller <nefuller.business@gmail.com>
2015-09-05 21:23:25 +0800

    Improved target indicators

--- Assets/Scripts/TargetIndicator.cs
+++ Assets/Scripts/TargetIndicator.cs
@@ -6,7 +6,7 @@ using System;
 public class TargetIndicator : MonoBehaviour {
     
     // public float IndicatorOffset = 0.05f;
-    public float clampBorderSize = 0.05f; 
+    public int ScreenBorderSize = 20; 
     public Material NeutralMaterial;
     public Material FriendlyMaterial;
     public Material EnemyMaterial;
@@ -57,6 +57,8 @@ public class TargetIndicator : MonoBehaviour {
 
 	void Update ()
     {
+        // If the target assigned to this target indicator has been destroyed, 
+        // destroy the target indicator and target information objects too.
         if (m_target == null)
         {
             Destroy(m_targetInfo.gameObject, 0);
@@ -64,160 +66,98 @@ public class TargetIndicator : MonoBehaviour {
             return;
         }
 
+        // Determine if the target is in front of, or behind, the player.
         bool targetInFront = false;
-        Vector3 direction = (m_target.position - transform.position).normalized;
-        float dot = Vector3.Dot(direction, transform.forward);
+        Vector3 targetDir = (m_target.position - m_player.position).normalized;
+        float targetDist = (m_target.position - m_player.position).magnitude;
+        float dot = Vector3.Dot(targetDir, transform.forward);
         if (dot > 0)
         {
             targetInFront = true;
         }
 
-        if (targetInFront)
+        // Determine if the target is on the screen.
+        bool targetOnScreen = false;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(m_target.position);
+        if (targetInFront &&
+            screenPoint.x > ScreenBorderSize &&
+            screenPoint.y > ScreenBorderSize &&
+            screenPoint.x < Screen.width - ScreenBorderSize && 
+            screenPoint.y < Screen.height - ScreenBorderSize)
         {
-
+            targetOnScreen = true;
         }
 
-        // Get angle from forward vector to target.
-        float angle = Vector3.Angle(m_player.forward, m_target.position );
-
-
-        Vector3 screenPoint = Camera.main.WorldToScreenPoint(m_target.position);
-        if (targetInFront && screenPoint.x > 0 + 20 && screenPoint.y > 0 + 20 &&
-            screenPoint.x < Screen.width - 20 && screenPoint.y < Screen.height -20)
+        if(targetOnScreen)
         {
-            Vector3 displacement = (m_target.transform.position -
-                transform.parent.position).normalized;
-            transform.position = transform.parent.position + displacement;
-            m_targetInfo.transform.position = Camera.main.WorldToScreenPoint(transform.position);
-            m_targetInfo.GetComponent<Text>().text = m_target.GetComponent<Target>().Label + "\n" + (m_target.transform.position -
-               transform.parent.position).magnitude.ToString("n2") + "\n" + m_target.GetComponent<Target>().Health;
-
+            // Update indicator position.
+            transform.position = m_player.position + targetDir;
+            // Update and reposition target information.
+            m_targetInfo.position = Camera.main.WorldToScreenPoint(transform.position);
+            m_targetInfo.GetComponent<Text>().text = m_targetScript.Label +
+                "\n" + targetDist.ToString("n2") + "\n" + m_targetScript.Health;
         }
         else
         {
+            // Position at edge of screen in the direction of the target.
             // Cast ray from target onto sphere collider at players 0, 0, 1.
             RaycastHit sphereHit;
             Physics.Raycast(m_target.position, (GameObject.Find("SphereCollider").transform.position -
                     m_target.position).normalized, out sphereHit, float.MaxValue, m_sphereColliderMask);
             if (sphereHit.collider != null)
             {
-                // Cast ray from target onto sphere collider hit point to plane
+                // Cast ray from target from sphere collider hit point to plane
                 // collider at players 0, 0, 1.
+
                 RaycastHit boxHit;
-                Physics.Raycast(sphereHit.point, targetInFront ? transform.parent.forward : -transform.parent.forward,
+                Physics.Raycast(sphereHit.point, transform.parent.forward,
                     out boxHit, float.MaxValue, m_boxColliderMask);
                 if (boxHit.collider != null)
                 {
-                    //Vector3 dd = (boxHit.point - transform.position).normalized;
-                    //transform.position = boxHit.point;
-                    Vector3 displacement = (boxHit.point -
-                        transform.parent.position).normalized * 2;
 
+                    Vector3 position = m_player.position + (boxHit.point - m_player.position).normalized;
+                    Vector3 screenPosition = Camera.main.WorldToScreenPoint(position);
+                    Vector3 screenOrigin = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+                    Vector3 screenEdgePosition = (screenPosition - screenOrigin) * 10f;
+                    screenEdgePosition = new Vector3(
+                        Mathf.Clamp(screenEdgePosition.x, ScreenBorderSize, Screen.width - ScreenBorderSize),
+                        Mathf.Clamp(screenEdgePosition.y, ScreenBorderSize, Screen.height - ScreenBorderSize), 1);
+                    transform.position = Camera.main.ScreenToWorldPoint(screenEdgePosition);
 
-                    //float a = Vector3.Angle(transform.parent.right, displacement) * Mathf.Deg2Rad;
-                    //double x = 0.5f * Math.Cos(a) + transform.parent.position.x;
-                    //double y = 0.5f * Math.Sin(a) + transform.parent.position.y;
-                    //displacement = new Vector3((float)x, (float)y, displacement.z);
-
-                    transform.position = transform.parent.position + displacement;
                     m_targetInfo.transform.position = Camera.main.WorldToScreenPoint(transform.position);
                     m_targetInfo.GetComponent<Text>().text = m_target.GetComponent<Target>().Label + "\n" + (m_target.transform.position -
                        transform.parent.position).magnitude.ToString("n2") + "\n" + m_target.GetComponent<Target>().Health;
                 }
                 else
                 {
-                           Physics.Raycast(sphereHit.point, targetInFront ? -transform.parent.forward : transform.parent.forward,
-                    out boxHit, float.MaxValue, m_boxColliderMask);
-                           if (boxHit.collider != null)
-                           {
-                               Vector3 displacement = (boxHit.point -
-                                   transform.parent.position).normalized * 2;
-                               //float a = Vector3.Angle(transform.parent.right, displacement) * Mathf.Deg2Rad;
-                               //double x = 1 * Math.Cos(a) + transform.parent.position.x;
-                               //double y = 1 * Math.Sin(a) + transform.parent.position.y;
-                               //displacement = new Vector3((float)x, (float)y, displacement.z);
+                    Physics.Raycast(sphereHit.point, -transform.parent.forward, out boxHit, float.MaxValue, m_boxColliderMask);
+                    if (boxHit.collider != null)
+                    {
+                        Vector3 position = m_player.position + (boxHit.point - m_player.position).normalized;
+                        Vector3 screenPosition = Camera.main.WorldToScreenPoint(position);
+                        Vector3 screenOrigin = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+                        Vector3 screenEdgePosition = (screenPosition - screenOrigin) * 10f;
+                        screenEdgePosition = new Vector3(
+                            Mathf.Clamp(screenEdgePosition.x, ScreenBorderSize, Screen.width - ScreenBorderSize),
+                            Mathf.Clamp(screenEdgePosition.y, ScreenBorderSize, Screen.height - ScreenBorderSize), 1);
+                        transform.position = Camera.main.ScreenToWorldPoint(screenEdgePosition);
 
-                               transform.position = transform.parent.position + displacement;
-                               m_targetInfo.transform.position = Camera.main.WorldToScreenPoint(transform.position);
-                               m_targetInfo.GetComponent<Text>().text = m_target.GetComponent<Target>().Label + "\n" + (m_target.transform.position -
-                                  transform.parent.position).magnitude.ToString("n2") + "\n" + m_target.GetComponent<Target>().Health;
-                           }
+                        m_targetInfo.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+                        m_targetInfo.GetComponent<Text>().text = m_target.GetComponent<Target>().Label + "\n" + (m_target.transform.position -
+                            transform.parent.position).magnitude.ToString("n2") + "\n" + m_target.GetComponent<Target>().Health;
+                    }
                 }
             }
         }
-
-
 	}
 }
 
 
+                   //Vector3 displacement = (boxHit.point -
+                   //                transform.parent.position).normalized;
+                   //            //float a = Vector3.Angle(transform.parent.right, displacement) * Mathf.Deg2Rad;
+                   //            //double x = 1 * Math.Cos(a) + transform.parent.position.x;
+                   //            //double y = 1 * Math.Sin(a) + transform.parent.position.y;
+                   //            //displacement = new Vector3((float)x, (float)y, displacement.z);
 
-
-
-//// if out of view indicator mapped to circle
-
-//// Get angle from forward vector to target.
-//float angle = Vector3.Angle(m_player.forward, m_target.position -  transform.parent.position);
-
-//// If angle outside hud
-
-//if (angle < -30 || angle > 30)
-//{
-
-
-
-//    // Is target in front of situation box collider origin or behind?
-//    bool inFront = false;
-//    bool onSide = false;
-//    Vector3 direction = (m_target.position - transform.position).normalized;
-//    float dot = Vector3.Dot(direction, transform.forward);
-//    if(dot < 0.1)
-//    {
-//        inFront = false;
-//    }
-//    else if(dot > 0.1)
-//    {
-//        inFront = true;
-//    }
-//    else 
-//    {
-//        onSide = true;
-//    }
-
-
-//    // Cast ray from target onto sphere collider at players 0, 0, 1.
-//    RaycastHit sphereHit;
-//    Physics.Raycast(m_target.position, 
-//        GameObject.Find("SphereCollider").transform.position -
-//            m_target.transform.position, out sphereHit, float.MaxValue, m_sphereColliderMask);
-//    if (sphereHit.collider != null)
-//    {
-//        if(onSide)
-//        {
-//            Vector3 dd = (sphereHit.point - transform.position).normalized;
-//            transform.position = sphereHit.point;
-//        }
-//        else
-//        {
-//            // Cast ray from target onto sphere collider hit point to plane
-//            // collider at players 0, 0, 1.
-//            RaycastHit boxHit;
-//            Physics.Raycast(sphereHit.point, inFront ? transform.forward : -transform.forward, 
-//                out boxHit, float.MaxValue, m_boxColliderMask);
-//            if (boxHit.collider != null)
-//            {
-//                //Vector3 dd = (boxHit.point - transform.position).normalized;
-//                //transform.position = boxHit.point;
-//                Vector3 displacement = (boxHit.point -
-//                    transform.parent.position).normalized;
-//                transform.position = transform.parent.position + displacement;
-//            }
-//        }
-//    }
-
-//    // flatten point on unit sphere
-
-//    // scale point onto unit sphere
-
-//    // update position
-//}
+                   //            transform.position = transform.parent.position + displacement;
